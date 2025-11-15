56f16f03fc17f0e81e331726c4bc834674edfb22
Theo Roweth <theoroweth@gmail.com>
2025-11-15 16:43:12 +0000

    more attempts

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,3 @@
-# Create the application user
-CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
-GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
 
 # Create database script for Berties books
 
